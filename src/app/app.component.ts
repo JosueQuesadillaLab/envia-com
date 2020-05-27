5967dfcd8d597b50b5f19dee6ed654adda0f2b89
Josué Casasola <j.casasola.lopez@gmail.com>
2020-05-27 09:09:22 -0500

    gitflow-feature-stash: view-products 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { ProductsService } from './services/products.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'smartstore';
+export class AppComponent implements OnInit , OnDestroy{
+
+  orderSubscription : Subscription = new Subscription();
+
+  constructor(
+    private _productService : ProductsService
+  ) { }
+
+  ngOnInit() {
+
+    this.orderSubscription = this._productService.getOrder().subscribe(
+      resp => console.log(resp)
+    );
+
+  }
+
+
+  ngOnDestroy(){
+
+    this.orderSubscription.unsubscribe();
+
+  }
+
+
 }
